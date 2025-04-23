b5ad27926c231eb315aa7a640216c06335529542
nevhutalubryan <nevhutalubryan@gmail.com>
2025-04-23 13:36:29 +0200

    verifyOpt Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/auth/loginPage";
 import DashboardPage from "./pages/lecturer/lecturerDashboard/dashboard";
 import ResetPage from "./pages/auth/resetPage";
+import OtpPage from "./pages/auth/verifyOtp";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/reset" element={<ResetPage />} />
+      <Route path="/verify" element={<OtpPage />} />
     </Routes>
+
     </>
   );
 }
