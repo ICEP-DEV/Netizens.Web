b41dbd4c3ac0e4a3f499f9361534a2eff8b431a1
Vhahangwele07 <vmutheiwana01@gmail.com>
2025-04-23 13:16:18 +0200

    added a table on lecture dashboard

--- src/pages/lecturer/lecturerDashboard/dashboard.jsx
+++ src/pages/lecturer/lecturerDashboard/dashboard.jsx
@@ -25,18 +25,40 @@ const DashboardPage = () => {
            </div>
 
             <div className="item">
-              <h2>Submission Status</h2>
-              <p>All reports are up to date</p>
+              <h2>Submission <br /> Status</h2>
+              <p>All reports <br/>are up to date</p>
             </div>
             <div className="item">
-              <h1>Upcoming in:</h1>
+              <h1>Upcoming <br /> in:</h1>
               
             </div>
           </div>
 
+          
           <div className="table-section">
-            
-          </div>
+  <h2 className="table-heading">Recent Submissions Status</h2>
+  <table>
+    <thead>
+      <tr>
+        
+        <th>Submission Date:</th>
+        <th>Review Status:</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        
+        <td>2025-04-20</td>
+        <td>Reviewed</td>
+      </tr>
+      <tr>
+        
+        <td>2025-04-27</td>
+        <td>Pending</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
         </div>
       </div>
     </div>
