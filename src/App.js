80de56ad7d54fd4f7f869eb11db267b0a6293083
MOKHOMOLEKS4 <mokhomoleks4@gmail.com>
2025-04-24 09:58:15 +0200

    Revert "Merge branch 'Auth_v1' of https://github.com/ICEP-DEV/Netizens.Web into Auth_v1"
    This reverts commit 6ada2ef723dc500a78ab4bc0f68756b823a1eebf, reversing
    changes made to 6e73548b3d1d6fde8e60c323b294d0b86c38acf8.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-
-import { Route, Routes } from "react-router-dom";
-import { LoginPage } from "./pages/auth/loginPage";
-import DashboardPage from "./pages/lecturer/lecturerDashboard/dashboard";
-import ResetPage from "./pages/auth/resetPage";
-import OtpPage from "./pages/auth/verifyOtp";
-
-import VerifyOtpPage from "./pages/auth/verifyOtpPage/VerifyOtpPage";
-
-import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage/ForgotPasswordPage";
-import SetPassword from "./pages/auth/SetPassword";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-
-
-function App() {
-
-
-
   return (
-    <>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/reset" element={<ResetPage />} />
-      <Route path="/verify" element={<OtpPage />} />
-
-      <Route path="/otp" element={<VerifyOtpPage />} />
-      <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-
-      <Route path="/setpassword" element={<SetPassword/>} />
-  
-
-    </Routes>
-
-    </>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-}
+
 export default App;
-
-
