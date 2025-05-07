c0d03190b101a1213b027e219da42b7f56054271
Vhahangwele07 <vmutheiwana01@gmail.com>
2025-05-07 10:23:03 +0200

    updated student dashboard

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './sidebar.css';
+import Icon from '../assets/TUTicon1.jpeg';
 
 const Sidebar = () => {
   return (
     <div className="sidebar-content">
       
       <ul>
+        <img className="logo" alt="TUT icon" src={Icon}/>
+        <li>Dashboard</li>
         <li>New Report</li>
         <li>My Report </li>
         
