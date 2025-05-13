45c6bc7c9717d3467874cd1eb15dd264306d9ce2
Vhahangwele07 <vmutheiwana01@gmail.com>
2025-05-13 12:24:00 +0200

    connected the logout button to backend

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import "./sidebar.css";
 import Icon from "../assets/TUTicon1.jpeg";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const Sidebar = () => {
-  const navigate = useNavigate();
-
+  
+ const navigate = useNavigate();
   const handleLogout = () => {
-    const confirmed = window.confirm("Are you sure you want to logout?");
-    if (confirmed) {
-      localStorage.clear();
+    axios.post(
+      "http://localhost:5041/api/Auth/Logout",
+      {},
+      {
+        withCredentials: true,
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((response) => {
+      toast.success(response.data.message) ;
       navigate("/");
-    }
+    })
+    .catch((error) => {
+      toast.error("Unable to logout",error);
+    });
+    
   };
   return (
     <div className="sidebar-contents">
+      <Toaster/>
       <ul>
         <img className="sidebar-logo" alt="TUT icon" src={Icon} />
         <Link className="sidebar-link">Dashboard</Link>
-        <Link to="/weekly-report" className="sidebar-link">New Report</Link>
-        <Link to="/report-history" className="sidebar-link">My Report </Link>
-        <Link to="/report-history" className="sidebar-link">My Profile </Link>
+        <Link to="/weekly-report" className="sidebar-link">
+          New Report
+        </Link>
+        <Link to="/report-history" className="sidebar-link">
+          My Report{" "}
+        </Link>
+        <Link to="/report-history" className="sidebar-link">
+          My Profile{" "}
+        </Link>
       </ul>
       <button onClick={handleLogout}>LOGOUT</button>
     </div>
